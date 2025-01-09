38f33d63a50bcad369949c096f38172b2ad9d8c1
ChristianGinko <christianginko@gmail.com>
2025-01-09 14:42:47 -0300

    Cambios para Splash Screen

---
+++ app/src/main/java/com/example/miprimeraapp/Lista.kt
@@ -0,0 +1,18 @@
+package com.example.miprimeraapp
+
+object Lista {
+    val messages = listOf(
+        MyMessage(R.drawable.new_jedi_order,"New Jedi Order"),
+        MyMessage(R.drawable.separatists,"Separatists"),
+        MyMessage(R.drawable.sith_empire,"Sith Empire"),
+        MyMessage(R.drawable.rebel_alliance,"Rebel Alliance"),
+        MyMessage(R.drawable.new_republic,"New Republic"),
+        MyMessage(R.drawable.galactic_republic,"Galactic Republic"),
+        MyMessage(R.drawable.old_republic,"Old Republic"),
+        MyMessage(R.drawable.mandalorians,"Mandalorians"),
+        MyMessage(R.drawable.black_sun,"Black Sun"),
+        MyMessage(R.drawable.galactic_senate,"Galactic Senate"),
+        MyMessage(R.drawable.galactic_empire,"Galactic Empire"),
+        MyMessage(R.drawable.orden_jedi,"Jedi Order")
+    )
+}
--- app/src/main/java/com/example/miprimeraapp/MainScreen.kt
+++ app/src/main/java/com/example/miprimeraapp/MainScreen.kt
@@ -29,21 +29,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
-private val messages: List<MyMessage> = listOf(
-    MyMessage(R.drawable.new_jedi_order,"New Jedi Order"),
-    MyMessage(R.drawable.separatists,"Separatists"),
-    MyMessage(R.drawable.sith_empire,"Sith Empire"),
-    MyMessage(R.drawable.rebel_alliance,"Rebel Alliance"),
-    MyMessage(R.drawable.new_republic,"New Republic"),
-    MyMessage(R.drawable.galactic_republic,"Galactic Republic"),
-    MyMessage(R.drawable.old_republic,"Old Republic"),
-    MyMessage(R.drawable.mandalorians,"Mandalorians"),
-    MyMessage(R.drawable.black_sun,"Black Sun"),
-    MyMessage(R.drawable.galactic_senate,"Galactic Senate"),
-    MyMessage(R.drawable.galactic_empire,"Galactic Empire"),
-    MyMessage(R.drawable.orden_jedi,"Jedi Order")
-)
+import com.example.miprimeraapp.Lista.messages
 
 @Composable
 fun MainScreen() {
